974fed90424978136e8ea30b0e28d62e16478a80
이진영 <jakerhee321@gmail.com>
2022-09-21 22:41:47 +0900

    Generate Random Number

--- Number_Guesser.py
+++ Number_Guesser.py
@@ -1,1 +1,18 @@
 import random
+
+upto = input("Type a number: ")
+
+if upto.isdigit():
+    upto = int(upto)
+
+    if upto <= 0:
+        print("Type in number larger than 0.")
+        quit()
+
+else:
+    print("Type in number.")
+    quit()
+
+random_number = random.randint(0, upto)
+
+
