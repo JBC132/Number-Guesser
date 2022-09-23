2a8f352df05b1cf18601abe196349eaf355c703f
이진영 <jakerhee321@gmail.com>
2022-09-23 11:41:55 +0900

    Number of guesses and wrapping up code

--- Number_Guesser.py
+++ Number_Guesser.py
@@ -14,9 +14,10 @@ else:
     quit()
 
 random_number = random.randint(0, upto)
-
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -27,7 +28,15 @@ while True:
 
     if user_guess == random_number:
         print("-----Correct.-----")
-        break
+        break    
     
+    elif user_guess > random_number:
+        print("------Wrong.------")
+        print("The number is smaller.")
+        
     else:
         print("------Wrong.------")
+        print("The number is bigger.")
+
+
+print("You got it in {} guesses".format(guesses))
